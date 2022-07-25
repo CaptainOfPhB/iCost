37ffd282ccdc9fd919d7061019c86e995e113152
jiangtao.dai <jiangtao.dai@aihuishou.com>
2022-07-25 17:20:26 +0800

    install vant and unplugin-vue-components

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import Components from 'unplugin-vue-components/vite';
 import PostcssPxToViewport from 'postcss-px-to-viewport';
+import { VantResolver } from 'unplugin-vue-components/resolvers';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [
+        VantResolver(),
+      ],
+    }),
+  ],
   css: {
     postcss: {
       plugins: [
