411430e324158482543615841dd839bae8bc7684
CaptainOfPhB <captainofphb@gmail.com>
2022-07-25 23:22:08 +0800

    install eslint and prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import eslint from 'vite-plugin-eslint'
 import Components from 'unplugin-vue-components/vite';
 import PostcssPxToViewport from 'postcss-px-to-viewport';
 import { VantResolver } from 'unplugin-vue-components/resolvers';
@@ -7,6 +8,7 @@ import { VantResolver } from 'unplugin-vue-components/resolvers';
 export default defineConfig({
   plugins: [
     vue(),
+    eslint(),
     Components({
       resolvers: [
         VantResolver(),
